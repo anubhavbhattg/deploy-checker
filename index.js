c80dbd3588a8b95e3ac5882989765aedc9bf3155
Anubhav Bhatt <anubhavbhatt.g@gmail.com>
2025-04-10 09:35:11 +0530

    Update for Service auto deploy

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Express Js is running at 3000 Port');
+    res.send('Express Js is running, and now service setup');
 });
 
 app.listen(PORT, () => {
